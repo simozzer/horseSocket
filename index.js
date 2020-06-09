ec5df49a9433fd1e276a59da21b79e09faabfcf2
i049605 <SimonMoscrop@sap.com>
2020-06-09 17:41:01 +0100

    remove redundant log message

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const WebSocket = require('ws')
+const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8081 })
+const wss = new WebSocket.Server({ port: 8081 });
 
 var connected = [];
 
 wss.on('connection', (ws) => {
     ws.on('message', (message) => {
-        console.log("message");
         wss.clients.forEach((client) => {
             client.send(message);
         })
